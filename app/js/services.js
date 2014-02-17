d4d051714893e989d5f9177715a98f0d84858862
Marc Von Ahn <marc.vonahn@adp.com>
2014-02-17 13:51:26 -0800

    progress

--- app/js/services.js
+++ app/js/services.js
@@ -5,7 +5,7 @@
 
 // Demonstrate how to register services
 // In this case it is a simple value service.
-angular.module('wlhssbApp.services', []).
-  value('version', '0.1');
+angular.module('wlhssbApp.services', [])
+    .value('version', '0.1');
 
 
