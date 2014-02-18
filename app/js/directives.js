8002fdbeb0b6c8eb050f71a0a61bc7230fab2fd7
Marc Von Ahn <marc.vonahn@adp.com>
2014-02-18 15:21:01 -0800

    Moving widgets to Directives

--- app/js/directives.js
+++ app/js/directives.js
@@ -8,4 +8,35 @@ angular.module('wlhssbApp.directives', []).
     return function(scope, elm, attrs) {
       elm.text(version);
     };
-  }]);
+  }])
+  .directive('upcomingEvents', function(){
+        return {
+            restrict: 'E',
+            templateUrl: '/partials/upcomingEvents.html',
+            replace: true,
+            controller: 'UpcomingEventController'
+        }
+    })
+    .directive('sideBarImages', function(){
+        return {
+            restrict: 'E',
+            templateUrl: '/partials/sideBarImages.html',
+            replace: true,
+            controller: 'MediaController'
+        }
+    })
+    .directive('navBar', function(){
+        return {
+            restrict: 'E',
+            templateUrl: '/partials/navBar.html',
+            replace: true,
+            controller: 'NavController'
+        }
+    })
+    .directive('flashStore', function(){
+        return {
+            restrict: 'E',
+            templateUrl: '/partials/flashStore.html',
+            replace: true
+        }
+    });
